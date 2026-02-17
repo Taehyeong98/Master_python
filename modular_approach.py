8ebae1b15f6be7806cf4350a8f24c88cdfea8a01
. Kim <kim@Mac.fritz.box>
2026-02-17 18:04:21 +0100

    modular approach is not finished yet

---
+++ modular_approach.py
@@ -0,0 +1,152 @@
+import roboticstoolbox as rtb
+import numpy as np
+import os
+from scipy.interpolate import CubicSpline
+from scipy.spatial.transform import Rotation as R
+from roboticstoolbox import Robot
+from spatialmath import SE3
+from spatialgeometry import Mesh
+from scipy.spatial.transform import Slerp
+import trimesh
+import pybullet as p
+import pybullet_data
+import time
+from pybullet_utils import bullet_client
+from ompl import base as ob
+from ompl import geometric as og
+import math
+import csv
+import random
+import io
+import sys
+
+matlab = "/Users/kim/Documents/MATLAB/New Folder/path_planning/"
+import fcl
+import qpsolvers
+
+# Load URDF of Robot
+robot = rtb.Robot.URDF(matlab + "imed_robot.urdf")
+
+endEffector = 'tool'  # name of end-effector link
+
+# Define weights for position (x,y,z) and orientation (roll, pitch, yaw)
+weights = np.array([0.5, 0.5, 0.5, 1, 1, 1])
+
+# --- Initialize configuration ---
+start_pose_deg = [0.1,-90.0, 90.0, -90.0, 0.0, -90.0, 0.0]  # Python list or numpy array
+q_start_prismatic = start_pose_deg[0]
+q_start_revolute = np.radians(start_pose_deg[1:7]) # Convert revolute joints (2–7) to radians
+start_pose_rad = np.concatenate(([q_start_prismatic], q_start_revolute)) # Combine back into one joint vector
+
+# park pose
+park_pose_deg = [1.4,-90.0, 90.0, -90.0, 0.0, -90.0, 0.0]
+q_park_prismatic = park_pose_deg[0]
+q_park_revolute = np.radians(park_pose_deg[1:7])
+park_pose_rad = np.concatenate(([q_park_prismatic], q_park_revolute))
+
+# goal pose
+goal_pos = [1.623, 0.577, 0.322]
+qx = R.from_rotvec(np.pi * np.array([1, 0, 0]))
+qy = R.from_rotvec((np.pi/4) * np.array([0, 1, 0]))
+goal_ori = (qy * qx).as_quat()  # returns [x,y,z,w]
+T_target = SE3()
+T_target.t = goal_pos
+T_target.R = R.from_quat(goal_ori).as_matrix()
+solver = robot.ikine_LM(Tep=T_target, mask=weights, joint_limits=True, method='sugihara', k=0.0001,
+                           q0=park_pose_rad)  # replace with your Python IK function
+print('goal_pose', solver.q)
+goal_pose_q = solver.q # 1x7
+
+#--------------------#
+#  Linear Movement   #
+#--------------------#
+allConfigTraj =[]
+linear_time = 10
+linear_movement = np.zeros((linear_time,len(start_pose_rad)))
+linear_movement[0:] = start_pose_rad
+x_start = start_pose_rad[0]
+x_goal = goal_pose_q[0]
+
+linear_distance = x_goal - x_start
+
+
+step = linear_distance / linear_time
+for i in range(linear_time):
+    # Prismatic joint moves linearly
+    x_current = x_start + step * i
+
+    # Revolute joints stay fixed
+    q_current = np.concatenate((
+        [x_current],
+        start_pose_rad[1:]
+    ))
+
+    linear_movement[i] = q_current
+
+#--------------------#
+#    GOAL REGION     #
+#--------------------#
+goal_time_joint = 10
+n_joints = len(start_pose_rad)
+goal_movement_joint = np.zeros((goal_time_joint,len(start_pose_rad)))
+add_time = goal_time_joint*2 # 90 deg for joint (2) and joint (3)
+
+# twist movement joint (2) and joint (3)
+ninety_rad = 90 * math.pi / 180
+twist_step = ninety_rad/goal_time_joint
+twist_start_2 = park_pose_rad[1]
+twist_start_3 = park_pose_rad[2]
+twist_movement_2 = np.zeros((goal_time_joint,len(start_pose_rad)))
+twist_movement_3 = np.zeros((goal_time_joint,len(start_pose_rad)))
+for j in range(goal_time_joint):
+    twist_movement_2[j]= twist_start_2 + twist_step * j
+    twist_movement_3[j] = twist_start_3 + twist_step * j
+
+goal_time = goal_time_joint*n_joints+add_time
+goal_movement = np.zeros((goal_time,len(start_pose_rad)))
+goal_distance =np.zeros(len(start_pose_rad))
+step_joint =np.zeros(len(start_pose_rad))
+goal_start = np.zeros(len(start_pose_rad))
+for i in range(n_joints):
+
+    if i == 1:
+        start = park_pose_rad[i] + ninety_rad
+    elif i == 2:
+        start = park_pose_rad[i] - ninety_rad
+    else:
+        start = park_pose_rad[i]
+
+    goal_distance = goal_pose_q[i] - start
+    step = goal_distance / (goal_time_joint - 1)
+
+    for j in range(goal_time_joint):
+        goal_movement_joint[j, i] = start + step * j
+
+#--------------------#
+#     Trajectory     #
+#--------------------#
+# end
+joint_1= goal_movement_joint[goal_time_joint-1,0]
+joint_2 =goal_movement_joint[goal_time_joint-1,1]
+joint_3= goal_movement_joint[goal_time_joint-1,2]
+joint_4 =goal_movement_joint[goal_time_joint-1,3]
+joint_5= goal_movement_joint[goal_time_joint-1,4]
+joint_6 =goal_movement_joint[goal_time_joint-1,5]
+joint_7= goal_movement_joint[goal_time_joint-1,6]
+
+
+allConfigTraj = np.zeros((goal_time, len(start_pose_rad)))
+
+for i in range(goal_time_joint):
+    # Combine prismatic joint + fixed revolute joints
+    step_1 = np.concatenate(([goal_movement_joint[i, 0]], park_pose_rad[1:]))
+    allConfigTraj[i, :] = step_1
+
+print(allConfigTraj)
+#allConfigTraj =np.rstack((linear_movment))
+
+
+
+
+
+
